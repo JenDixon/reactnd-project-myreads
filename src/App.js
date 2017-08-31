4600e56ec4e54affa2cd488554fb9362ccf75ef4
Jen Dixon <jdixon@theladders.com>
2017-08-31 18:17:20 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import * as BooksAPI from './BooksAPI';
-import './App.css';
-import Shelf from './Shelf';
-import Search from './Search';
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import Shelf from "./Shelf";
+import Search from "./Search";
 
 class BooksApp extends React.Component {
   state = {
@@ -13,93 +14,81 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     books: [],
-    showSearchPage: true
-  }
+    showSearchPage: false
+  };
 
   componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
   }
 
   updateShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf).then((books) =>  {
+    BooksAPI.update(book, shelf).then(books => {
       this.getBooks();
-    })
-  }
-
-  updateQuery = (event) => {
-    this.setState({ query: this.event.query })
-  }
+    });
+  };
 
   getBooks = () => {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
-  }
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  };
 
   render() {
-
-  const BOOK_TITLE_MAP = {
-    "Currently Reading": "currentlyReading",
-    "Want to Read": "wantToRead",
-    "Read": "read"
-  }
+    const BOOK_TITLE_MAP = {
+      "Currently Reading": "currentlyReading",
+      "Want to Read": "wantToRead",
+      Read: "read"
+    };
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-          <Search />
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        <Route path="/search" component={Search} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <Link to="/search">Search</Link>
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <Shelf
+                    books={this.state.books}
+                    title="Currently Reading"
+                    bookTitleMap={BOOK_TITLE_MAP}
+                    onMoveBook={this.updateShelf}
+                  />
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input
-                  type="text"
-                  placeholder="Search by title or authorx"
-                  value={ this.state.query }
-                  onChange={ (event) => { this.updateQuery(event.target.value)  }}/>
+                  <Shelf
+                    books={this.state.books}
+                    title="Want to Read"
+                    bookTitleMap={BOOK_TITLE_MAP}
+                    onMoveBook={this.updateShelf}
+                  />
 
+                  <Shelf
+                    books={this.state.books}
+                    title="Read"
+                    bookTitleMap={BOOK_TITLE_MAP}
+                    onMoveBook={this.updateShelf}
+                  />
+                </div>
+              </div>
+              <div className="open-search">
+                <a onClick={() => this.setState({ showSearchPage: true })}>
+                  Add a book
+                </a>
               </div>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid">
-
-              </ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-            <div>
-
-              <Shelf books={ this.state.books } title="Currently Reading" bookTitleMap={ BOOK_TITLE_MAP } onMoveBook={this.updateShelf} />
-
-              <Shelf books={ this.state.books } title="Want to Read" bookTitleMap={ BOOK_TITLE_MAP } onMoveBook={this.updateShelf} />
-
-              <Shelf books={ this.state.books } title="Read" bookTitleMap={ BOOK_TITLE_MAP } onMoveBook={this.updateShelf} />
-
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
